07b40e56bc8acb0139ffe288a8083f5b2aaf47bb
sheikh-20 <sheikhzs1032@gmail.com>
2025-03-03 20:13:03 +0530

    home top appbar added

---
+++ Habitly/view/dashboard/home/OverallView.swift
@@ -0,0 +1,24 @@
+//
+//  OverallView.swift
+//  Habitly
+//
+//  Created by Sheikh Mohideen on 03/03/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct OverallView: View {
+    
+    var body: some View {
+        VStack {
+            Text("Overall View") // Your main content
+        }
+    }
+}
+
+struct OverallView_Previews: PreviewProvider {
+    static var previews: some View {
+        OverallView()
+    }
+}
